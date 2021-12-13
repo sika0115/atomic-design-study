cf7d123db6cda8e0735acada2a4e9daba69e2e97
KarinShoji <36564217+sika0115@users.noreply.github.com>
2021-12-13 18:16:49 +0900

    Contextでのstate管理（固定)

--- src/components/molecules/user/UserIconWithName.jsx
+++ src/components/molecules/user/UserIconWithName.jsx
@@ -1,7 +1,12 @@
+import React, { useContext } from "react";
 import styled from "styled-components";
+import { UserContext } from "../../../providers/UserProvider";
 
 export const UserIconWithName = (props) => {
   const { image, name, isAdmin } = props;
+  const context = useContext(UserContext);
+  console.log(context);
+
   return (
     <SContainer>
       <SImg height={160} weight={160} src={image} alt={name} />
